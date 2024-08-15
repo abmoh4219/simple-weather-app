bfb9f2b9610e7b92394a33417879fed9cffe0f5e
Abdelah <abdelahmohammed0919@gmail.com>
2024-08-15 08:57:53 +0300

    added js file

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ weatherForm.addEventListener("submit",async event =>{
 
 
 
+
 async function getWeatherData(city) {
     
     const apiUrl='https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+apiKey;
